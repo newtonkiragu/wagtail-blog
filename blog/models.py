452c88d2e8dbbdbb54bc7bad5dbf464d8067d4bd
Newton Karanu <karanunewton4@gmail.com>
2019-11-27 17:03:45 +0300

    added image model

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 
 class BlogIndexPage(Page):
@@ -20,8 +24,29 @@ class BlogPage(Page):
     body = RichTextField(blank=True)
     date = models.DateField("Post date")
 
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
         FieldPanel('date'),
         FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+
+class BlogPageGalleryIMage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
